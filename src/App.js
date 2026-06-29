12ebd0cd4f70a31241f01ec6f26e478a03725a95
Faruk Alparizi <farukalparizi@example.com>
2026-06-29 16:35:42 +0700

    Initial commit with Tailwind UI setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DanantaraPmo from './danantara-pmo'; // Menghubungkan ke file baru kamu
+
+function App() {
+  return (
+    <div className="App">
+      <DanantaraPmo />
+    </div>
+  );
+}
+
+export default App;
